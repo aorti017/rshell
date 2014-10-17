c7efdf40ec0b6de1cb09699fd6e5957084ef3592
Alexander Ortiz <aorti017@ucr.edu>
2014-10-17 11:08:38 -0700

    Updated hw0.cpp: able to handle more connector cases

--- src/hw0.cpp
+++ src/hw0.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 bool conntest(string x){
     int i = x.size() - 1;
+    int y = 0;
     if(x == "&&" || x == ";" || x == "||"){
         //cout << "a" << endl;
         return true;
@@ -18,6 +19,9 @@ bool conntest(string x){
     else if(x.at(i) == ';' || x.at(i) == '&'
          || x.at(i) == '|')
     {
+        return true;
+    }
+    else if(x.at(y) == ';' || x.at(y) == '|' || x.at(y) == '&'){
         return true;
     }
     else{
@@ -48,51 +52,89 @@ int findend(string x){
     return -1;
 }
 
+bool beginswith(string x){
+    if(x.at(0) == ';' || x.at(0) == '&' || x.at(0) == '|'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+string chopFront(string x, vector<string> &v){
+    if(x.at(0) == '&' && x.at(1) == '&'){
+        v.push_back(x.substr(2,x.size()-1));
+        return x.substr(0, 2);
+    }
+    else if(x.at(0) == '|' && x.at(1) == '|'){
+        v.push_back(x.substr(2,x.size()-1));
+        return x.substr(0, 2);
+    }
+    else{
+        v.push_back(x.substr(1,x.size()-1));
+        return x.substr(0, 1);
+    }
+}
 
 bool run(char str[]){
-   char* pch;
-   bool sucs;
-   vector<string> cmd;
-   pch = strtok(str, " ");
+    char* pch;
+    bool sucs;
+    string connector;
+    vector<string> cmd;
+    vector<string> addIn;
+    pch = strtok(str, " ");
 
-   while(pch != "exit"){
-       if(pch == NULL || conntest(pch)){
-           if(pch != NULL && endswith(pch)){
-               string tmp = pch;
-	       int loc = findend(pch);
-	       if(loc > 0){
-               tmp = tmp.substr(0, loc);
-               cmd.push_back(tmp);
-	       }
-           }
-           int pid = fork();
-           if(pid == 0){
-               char* argc[cmd.size() + 1];
-                for(int i = 0 ; i < cmd.size(); i++ ){
-                	argc[i] = new char[cmd.at(i).size()];
-                	strcpy(argc[i], cmd.at(i).c_str());
+    while(pch != "exit"){
+        if(pch == NULL || conntest(pch)){
+            if(pch != NULL && (endswith(pch) || beginswith(pch))){
+                string tmp = pch;
+	            int loc = findend(pch);
+	            if(loc > 0){
+                    tmp = tmp.substr(0, loc);
+                    cmd.push_back(tmp);
+	            }
+                if(loc == 0){
+                    connector  = chopFront(pch, addIn);
+                    cout << connector << endl;
                 }
-                argc[cmd.size()] = NULL;
-               if(-1 ==  execvp(argc[0], argc)){
-                   perror("There was an error in execvp");
-               }
-           }
-           else{
-               wait(NULL);
-	       pch = strtok(NULL, " ");
-	       cmd.clear();
-	       if(pch == "exit")
-	       exit(1);
-               if(pch == NULL)
-	       return true;
-           }
+            }
+            int pid = fork();
+            if(pid == 0){
+                char* argc[cmd.size() + 1];
+                 for(int i = 0 ; i < cmd.size(); i++ ){
+                	 argc[i] = new char[cmd.at(i).size()];
+                	 strcpy(argc[i], cmd.at(i).c_str());
+                 }
+                 argc[cmd.size()] = NULL;
+                 if(-1 ==  execvp(argc[0], argc)){
+                    perror("execvp");
+                 }
+            }
+            else{
+                wait(NULL);
+	            pch = strtok(NULL, " ");
+	            cmd.clear();
+                for(int i = 0; i < addIn.size(); i++){
+                    cmd.push_back(addIn.at(i));
+                }
+                addIn.clear();
+	            if(pch == "exit"){
+	                exit(1);
+                }
+                if(pch == NULL && cmd.size() == 0){
+	                return true;
+                }
+            }
        }
        else{
-	   if(pch != " ")
-           cmd.push_back(pch);
+	       if(pch != " "){
+               cmd.push_back(pch);
+           }
            pch = strtok(NULL, " ");
-	   if(pch == "exit")
-	   exit(1);
+	       if(pch == "exit"){
+	           exit(1);
+           }
        }
    }
    return false;
@@ -103,12 +145,12 @@ bool run(char str[]){
 int main(){
    bool goon = true;
    while(goon){
-   string input;
-   cout << "$ ";
-   getline(cin, input);
-   char str[input.size()+1];
-   strcpy(str, input.c_str());
-   goon = run(str);
+       string input;
+       cout << "$ ";
+       getline(cin, input);
+       char str[input.size()+1];
+       strcpy(str, input.c_str());
+       goon = run(str);
    }
    return 0;
 }
