30cf6918440971ac00c05931120e2ff96b954df7
Alexander Ortiz <aorti017@ucr.edu>
2014-10-29 21:20:32 -0700

    -l now displays type and last modified date

--- src/hw0.cpp
+++ src/hw0.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <time.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <queue>
@@ -210,6 +212,45 @@ bool ls_parse(vector<string> cmd, bool flags[], vector<string> &files){
     return true;
 }
 
+void getInfo(string path, struct stat buf){
+    vector<string> mnth;
+    mnth.push_back("Jan");
+    mnth.push_back("Feb");
+    mnth.push_back("Mar");
+    mnth.push_back("Apr");
+    mnth.push_back("May");
+    mnth.push_back("Jun");
+    mnth.push_back("Jul");
+    mnth.push_back("Aug");
+    mnth.push_back("Sep");
+    mnth.push_back("Oct");
+    mnth.push_back("Nov");
+    mnth.push_back("Dec");
+    if(S_ISLNK(buf.st_mode)){
+        cout << "l";
+    }
+    else if(S_ISDIR(buf.st_mode)){
+        cout << "d";
+    }
+    else if(S_ISREG(buf.st_mode)){
+        cout << "-";
+    }
+    cout << " ";
+    time_t secs = buf.st_mtime;
+    struct tm * ptm;
+    ptm = localtime(&secs);
+    cout << mnth.at(ptm->tm_mon);
+    cout << " ";
+    cout << ptm->tm_mday;
+    cout << " ";
+    cout << ptm->tm_hour;
+    cout << ":";
+    cout << ptm->tm_min;
+    cout << " ";
+}
+
+
+
 void printLs(bool flags[], queue<string> paths, string path){
     if(paths.empty()){
         return;
@@ -225,11 +266,18 @@ void printLs(bool flags[], queue<string> paths, string path){
     }
     while((direntp = readdir(dirp))){
         ftmp = direntp->d_name;
+
+        string tmpPath = paths.front();
+        tmpPath.append("/");
+        tmpPath.append(ftmp);
+        stat(tmpPath.c_str(), &buf);
         if(flags[1] && flags [0]){
             //get and cout info
+            getInfo(tmpPath, buf);
         }
         if(flags[1] && !flags[0] && ftmp.at(0) != '.'){
             //get and cout info
+            getInfo(tmpPath, buf);
         }
         if(flags[0]){
             cout << direntp->d_name << endl;
@@ -237,10 +285,6 @@ void printLs(bool flags[], queue<string> paths, string path){
         else if(!flags[0] && ftmp.at(0) != '.'){
             cout << direntp->d_name << endl;
         }
-        string tmpPath = paths.front();
-        tmpPath.append("/");
-        tmpPath.append(ftmp);
-        stat(tmpPath.c_str(), &buf);
         if(S_ISDIR(buf.st_mode) && ftmp != "." && ftmp != ".." && flags[2]){
             if(tmp != ".git"){
                 if(flags[0]){
@@ -260,7 +304,6 @@ void printLs(bool flags[], queue<string> paths, string path){
     printLs(flags, paths, path);
     return;
 }
-
 
 //this function runs the command using fork and execvp
 //and returns once all commands from the entered char[]
